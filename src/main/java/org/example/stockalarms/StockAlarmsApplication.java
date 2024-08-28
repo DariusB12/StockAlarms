5af1898e97ec4ef9c01db88694a5072cad4d88d5
DariusName <bordeanu96darius@gmail.com>
2024-08-28 16:36:40 +0300

    App Initialization

---
+++ src/main/java/org/example/stockalarms/StockAlarmsApplication.java
@@ -0,0 +1,12 @@
+package org.example.stockalarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlarmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockAlarmsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/stockalarms/model/User.java
@@ -0,0 +1,36 @@
+package org.example.stockalarms.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public User(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+}
